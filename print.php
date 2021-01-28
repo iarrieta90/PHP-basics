dcf24ec40c1754aa9a0a66ea9b27057521c7b672
iarrieta90 <irati_arrieta90@hotmail.com>
2021-01-28 16:45:14 +0100

    Print.php page instructions done

---
+++ print.php
@@ -0,0 +1,13 @@
+<?php
+
+// Generate an instruction that makes use of "echo"
+echo "<p>Hello world with echo</p>";
+
+//Generate an instruction that makes use of "print"
+print "<p>Hello world with print</p>";
+
+//Generate an instruction that makes use of "print_r"
+$a = array("first position","second position","third position");
+print_r($a);
+
+?>
